b8a57f5bdc58bd5e4c37a21e7b22c2cea7f03b75
Nur Ihsan <ihsan@fas.id>
2024-01-03 06:56:28 +0800

    update firebase

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,15 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCNy7vVM8xqlHHrgKn1bBPpFbKKBct0f6Q",
-    authDomain: "speedcontrol-ba2ed.firebaseapp.com",
-    projectId: "speedcontrol-ba2ed",
-    storageBucket: "speedcontrol-ba2ed.appspot.com",
-    messagingSenderId: "273985573107",
-    appId: "1:273985573107:web:3bf179f45b91ba7777a786",
-    measurementId: "G-Z9CHGJ0YKT"
-  };
+  apiKey: "AIzaSyClf64ia_1t1GoQh4CjLzvHxQ0N_vKdnDE",
+  authDomain: "tesiot-6c1e9.firebaseapp.com",
+  databaseURL: "https://tesiot-6c1e9-default-rtdb.firebaseio.com",
+  projectId: "tesiot-6c1e9",
+  storageBucket: "tesiot-6c1e9.appspot.com",
+  messagingSenderId: "28467746570",
+  appId: "1:28467746570:web:d2e75f893462a088610cf3",
+  measurementId: "G-DD3DZ2MNY8"
+};
 
 const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app);
