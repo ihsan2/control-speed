87140709092c3b71046fe0f9da70a7ed06bd5fd1
Nur Ihsan <ihsan@fas.id>
2024-01-03 07:42:22 +0800

    Update3

--- src/index.js
+++ src/index.js
@@ -24,14 +24,13 @@ import "assets/scss/now-ui-dashboard.scss?v1.5.0";
 import "assets/css/demo.css";
 
 import AdminLayout from "layouts/Admin.js";
-import { HashRouter } from "react-router-dom/cjs/react-router-dom.min";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <HashRouter>
+  <BrowserRouter basename="/React">
     <Switch>
       <Route path="/admin" render={(props) => <AdminLayout {...props} />} />
     </Switch>
-  </HashRouter>
+  </BrowserRouter>
 );
